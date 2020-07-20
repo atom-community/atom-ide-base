6aff6936200a4779dbb04288ec7aa6fd12bae7ce
aminya <aminyahyaabadi74@gmail.com>
2020-07-20 18:47:07 -0500

    Revert "add install-package-deps to postinstall"
    This reverts commit a63742fd9050c9180e2ea9c4eea3ca263716397b.

--- script/install-package-deps.js
+++ script/install-package-deps.js
@@ -4,6 +4,6 @@ const pkg = require("../package.json");
 if (pkg["package-deps"]) {
   const deps = Array.from(pkg["package-deps"]);
   for (const dep of deps) {
-    execSync(`apm install ${dep}`, {stdio: "inherit"});
+    execSync(`apm install ${dep}`);
   }
 }
