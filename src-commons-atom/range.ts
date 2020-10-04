43191ee5125c53bc05db4c09120ddbaa17951463
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-10-04 17:45:52 -0500

    chore: format

--- src-commons-atom/range.ts
+++ src-commons-atom/range.ts
@@ -1,5 +1,5 @@
-import {TextEditor, TextEditorComponent, TextBuffer, Range, Point} from 'atom';
-import invariant from 'assert';
+import { TextEditor, TextEditorComponent, TextBuffer, Range, Point } from "atom"
+import invariant from "assert"
 
 /**
  * Finds the word at the position. You can either provide a word regex yourself,
@@ -10,30 +10,30 @@ import invariant from 'assert';
 export function wordAtPosition(
   editor: TextEditor,
   position: Point,
-  wordRegex?: RegExp | {includeNonWordCharacters: boolean},
-): {wordMatch: Array<string>, range: Range} | null {
-  let wordRegex_;
+  wordRegex?: RegExp | { includeNonWordCharacters: boolean }
+): { wordMatch: Array<string>; range: Range } | null {
+  let wordRegex_
   if (wordRegex instanceof RegExp) {
-    wordRegex_ = wordRegex;
+    wordRegex_ = wordRegex
   } else {
     // What is the word regex associated with the position? We'd like to use
     // Cursor.wordRegExp, except that function gets the regex associated
     // with the editor's current cursor while we want the regex associated with
     // the specific position. So we re-implement it ourselves...
     // @ts-ignore: https://github.com/atom/atom/blob/aa3c34bedb361e09a5068dce9620b460a20ca3fb/src/text-editor.js#L5032
-    const nonWordChars: string = editor.getNonWordCharacters(position);
-    const escaped = nonWordChars.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
+    const nonWordChars: string = editor.getNonWordCharacters(position)
+    const escaped = nonWordChars.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&")
     // We copied this escaping regex from Cursor.wordRegexp, rather than
     // using the library function 'escapeStringRegExp'. That's because the
     // library function doesn't escape the hyphen character and so is
     // unsuitable for use inside a range.
-    let r = `^[\t ]*$|[^\\s${escaped}]+`;
+    let r = `^[\t ]*$|[^\\s${escaped}]+`
     if (wordRegex == null || wordRegex.includeNonWordCharacters) {
-      r += `|[${escaped}]+`;
+      r += `|[${escaped}]+`
     }
-    wordRegex_ = new RegExp(r, 'g');
+    wordRegex_ = new RegExp(r, "g")
   }
-  return wordAtPositionFromBuffer(editor.getBuffer(), position, wordRegex_);
+  return wordAtPositionFromBuffer(editor.getBuffer(), position, wordRegex_)
 }
 
 /**
@@ -47,35 +47,28 @@ export function wordAtPosition(
  *   defaults to first non-whitespace character
  * @return Range  the trimmed range
  */
-export function trimRange(
-  editor: TextEditor,
-  rangeToTrim: Range,
-  stopRegex: RegExp = /\S/,
-): Range {
-  const buffer = editor.getBuffer();
-  let {start, end} = rangeToTrim;
-  buffer.scanInRange(stopRegex, rangeToTrim, ({range, stop}) => {
-    start = range.start;
-    stop();
-  });
-  buffer.backwardsScanInRange(stopRegex, rangeToTrim, ({range, stop}) => {
-    end = range.end;
-    stop();
-  });
-  return new Range(start, end);
+export function trimRange(editor: TextEditor, rangeToTrim: Range, stopRegex: RegExp = /\S/): Range {
+  const buffer = editor.getBuffer()
+  let { start, end } = rangeToTrim
+  buffer.scanInRange(stopRegex, rangeToTrim, ({ range, stop }) => {
+    start = range.start
+    stop()
+  })
+  buffer.backwardsScanInRange(stopRegex, rangeToTrim, ({ range, stop }) => {
+    end = range.end
+    stop()
+  })
+  return new Range(start, end)
 }
 
-function getSingleWordAtPosition(
-  editor: TextEditor,
-  position: Point,
-): string | null {
-  const match = wordAtPosition(editor, position);
+function getSingleWordAtPosition(editor: TextEditor, position: Point): string | null {
+  const match = wordAtPosition(editor, position)
   // We should only receive a single identifier from a single point.
   if (match == null || match.wordMatch.length !== 1) {
-    return null;
+    return null
   }
 
-  return match.wordMatch[0];
+  return match.wordMatch[0]
 }
 
 /**
@@ -86,20 +79,17 @@ function getSingleWordAtPosition(
  *   from
  * @param event   the MouseEvent containing the screen position of the click
  */
-export function getWordFromMouseEvent(
-  editor: TextEditor,
-  event: MouseEvent,
-): string | null {
+export function getWordFromMouseEvent(editor: TextEditor, event: MouseEvent): string | null {
   // We can't immediately get the identifier right-clicked on from
   // the MouseEvent. Using its target element content would work in
   // some cases but wouldn't work if there was additional content
   // in the same element, such as in a comment.
   // @ts-ignore: https://github.com/atom/atom/blob/aa3c34bedb361e09a5068dce9620b460a20ca3fb/src/text-editor.js#L5075
-  const component: TextEditorComponent = editor.getElement().component;
-  invariant(component);
+  const component: TextEditorComponent = editor.getElement().component
+  invariant(component)
   // This solution doesn't feel ideal but it is the way hyperclick does it.
-  const point = component.screenPositionForMouseEvent(event);
-  return getSingleWordAtPosition(editor, point);
+  const point = component.screenPositionForMouseEvent(event)
+  return getSingleWordAtPosition(editor, point)
 }
 
 /**
@@ -111,68 +101,55 @@ export function getWordFromMouseEvent(
  *   triggered
  */
 export function getWordFromCursorOrSelection(editor: TextEditor): string | null {
-  const selection = editor.getSelectedText();
+  const selection = editor.getSelectedText()
   if (selection && selection.length > 0) {
-    return selection;
+    return selection
   }
 
   // There was no selection so we can go ahead and try the cursor position.
-  const point = editor.getCursorScreenPosition();
-  return getSingleWordAtPosition(editor, point);
+  const point = editor.getCursorScreenPosition()
+  return getSingleWordAtPosition(editor, point)
 }
-
 
 export function wordAtPositionFromBuffer(
   buffer: TextBuffer,
   position: Point,
-  wordRegex: RegExp,
-): {wordMatch: Array<string>, range: Range} | null {
-  const {row, column} = position;
-  const rowRange = buffer.rangeForRow(row);
-  let matchData: {match: Array<string>, range: Range} | null;
+  wordRegex: RegExp
+): { wordMatch: Array<string>; range: Range } | null {
+  const { row, column } = position
+  const rowRange = buffer.rangeForRow(row)
+  let matchData: { match: Array<string>; range: Range } | null
   // Extract the expression from the row text.
-  buffer.scanInRange(wordRegex, rowRange, data => {
-    const {range} = data;
-    if (
-      range.start.isLessThanOrEqual(position) &&
-      range.end.isGreaterThan(position)
-    ) {
-      matchData = data;
+  buffer.scanInRange(wordRegex, rowRange, (data) => {
+    const { range } = data
+    if (range.start.isLessThanOrEqual(position) && range.end.isGreaterThan(position)) {
+      matchData = data
     }
     // Stop the scan if the scanner has passed our position.
     if (range.end.column > column) {
-      data.stop();
+      data.stop()
     }
-  });
+  })
   // @ts-ignore (it is assigned above)
   if (matchData) {
     return {
       wordMatch: matchData.match,
       range: matchData.range,
-    };
+    }
   } else {
-    return null;
+    return null
   }
 }
 
 // Matches a regex on the text of the line ending at endPosition.
 // regex should end with a '$'.
 // Useful for autocomplete.
-export function matchRegexEndingAt(
-  buffer: TextBuffer,
-  endPosition: Point,
-  regex: RegExp,
-): string | null {
-  const line = buffer.getTextInRange([[endPosition.row, 0], endPosition]);
-  const match = regex.exec(line);
-  return match == null ? null : match[0];
+export function matchRegexEndingAt(buffer: TextBuffer, endPosition: Point, regex: RegExp): string | null {
+  const line = buffer.getTextInRange([[endPosition.row, 0], endPosition])
+  const match = regex.exec(line)
+  return match == null ? null : match[0]
 }
 
-export function isPositionInRange(
-  position: Point,
-  range: Range | Array<Range>,
-): boolean {
-  return Array.isArray(range)
-    ? range.some(r => r.containsPoint(position))
-    : range.containsPoint(position);
+export function isPositionInRange(position: Point, range: Range | Array<Range>): boolean {
+  return Array.isArray(range) ? range.some((r) => r.containsPoint(position)) : range.containsPoint(position)
 }
