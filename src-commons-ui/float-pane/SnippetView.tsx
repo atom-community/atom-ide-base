85cab5b71f410991b1e4bf7dbad4cf72360d30e6
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-10-08 04:03:59 -0500

    feat: use React for SnippetView

--- src-commons-ui/float-pane/SnippetView.tsx
+++ src-commons-ui/float-pane/SnippetView.tsx
@@ -1,44 +1,31 @@
-/**
- * a reference to the DOMpurify function to make safe HTML strings
- */
+import * as React from "react"
 import DOMPurify from "dompurify"
 import { MarkdownService } from "../../types-packages/main"
 import { getMarkdownRenderer } from "../MarkdownRenderer"
 
+interface Props {
+  snippet: string
+  containerClassName: string
+  contentClassName: string
+}
+
+interface State {}
+
 /**
- * an etch component that hosts a code snippet incl. syntax highlighting
+ * A React component that hosts a code snippet with syntax highlighting
  */
-export class SnippetView {
-  rootElement: HTMLElement
-
-  /**
-   * creates a snippet view component handing in the snippet
-   * @param {string} snippet the code snippet to be embedded
-   */
-  constructor({
-    snippet,
-    containerClassName,
-    contentClassName,
-  }: {
-    snippet: string
-    containerClassName: string
-    contentClassName: string
-  }) {
-    this.rootElement = document.createElement("div")
-    this.rootElement.classList.add(containerClassName)
-    if (snippet) {
-      const innerHTML = DOMPurify.sanitize(snippet)
-      this.rootElement.innerHTML = `
-        <div className='${contentClassName}'>${innerHTML}</div>`
-    }
-  }
-
-  /**
-   * returns the root element of the snippet view component
-   * @return {HTMLElement} the root element wrapping the HTML content
-   */
-  get element(): HTMLElement {
-    return this.rootElement
+export class SnippetView extends React.Component<Props, State> {
+  render() {
+    return (
+      <div className={this.props.containerClassName}>
+        <div
+          className={this.props.contentClassName}
+          dangerouslySetInnerHTML={{
+            __html: DOMPurify.sanitize(this.props.snippet),
+          }}
+        />
+      </div>
+    )
   }
 }
 
