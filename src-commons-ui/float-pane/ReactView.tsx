51f7a3b05c63109e32c9b6330d3077f22b8ccae9
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-10-08 02:09:12 -0500

    feat: fix types of ReactView

--- src-commons-ui/float-pane/ReactView.tsx
+++ src-commons-ui/float-pane/ReactView.tsx
@@ -1,31 +1,23 @@
 import * as React from "react"
 
+interface Props {
+  component: () => React.ReactElement
+  containerClassName: string
+  contentClassName: string
+}
+
+interface State {}
+
 /**
  * A react component that can host an externally given React component
  */
-export class ReactView extends React.Component {
-  containerClassName: string
-  contentClassName: string
-  props: {
-    component: () => React.ReactElement
-    containerClassName: string
-    contentClassName: string
-  }
-
+export class ReactView extends React.Component<Props, State> {
   render() {
     let children = null
     if (this.props.component) {
-      children = (
-        <div className={this.props.contentClassName}>
-          {this.props.component()}
-        </div>
-      )
+      children = <div className={this.props.contentClassName}>{this.props.component()}</div>
     }
 
-    return (
-      <div className={this.props.containerClassName}>
-        {children}
-      </div>
-    )
+    return <div className={this.props.containerClassName}>{children}</div>
   }
 }
