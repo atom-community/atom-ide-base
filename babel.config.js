9be475590dcd7ec4a880ef09576a08e76cd76c38
aminya <aminyahyaabadi74@gmail.com>
2020-10-12 23:51:52 -0500

    chore: ignore node_modules

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,6 @@ let plugins = []
 module.exports = {
   presets: presets,
   plugins: plugins,
-  exclude: "node_modules/**",
+  ignore: ["node_modules/**", "nuclide/dist-nuclide-commons/node_modules/**", "nuclide/dist-nuclide-commons-atom/node_modules/**", "nuclide/dist-nuclide-commons-ui/node_modules/**", "nuclide/dist-nuclide-debugger-common/node_modules/**", "nuclide/dist-nuclide-node-transpiler/node_modules/**"],
   sourceMap: "inline",
 }
