80fcfdbbd38489c9ce8cb3d3eab65387e74fd2ff
aminya <aminyahyaabadi74@gmail.com>
2020-10-16 19:12:27 -0500

    test: run tests using legacy jasmine + manually filter the test paths
    PR and its issue were rejected: 
    https://github.com/UziTech/atom-jasmine3-test-runner/issues/167

--- nuclide/atom-test-runners/jasmine-runner.js
+++ nuclide/atom-test-runners/jasmine-runner.js
@@ -1,11 +1,13 @@
 const { createRunner } = require("atom-jasmine3-test-runner")
 
+const fs = require("fs")
+
 // https://github.com/UziTech/atom-jasmine3-test-runner#api
 module.exports = createRunner({
   testPackages: process.env.APM_TEST_PACKAGES ? process.env.APM_TEST_PACKAGES.split(' ') : [],
-  suffix: "-test",
-  legacySuffix: "-test-v1",
-  testPaths: ["__tests__", "__atom_tests__"],
+  suffix: "-test-v3",
+  legacySuffix: "-test",
+  testPaths: ["__tests__", "__atom_tests__"].filter(dir => !fs.existsSync(process.cwd(), dir)),
   timeReporter: true,
   silentInstallation: true,
   // Extra Packages
