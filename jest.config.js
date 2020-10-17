4c55d386d5749dee46e3c37aee5ee6a41aa2d4b7
aminya <aminyahyaabadi74@gmail.com>
2020-10-17 16:31:14 -0500

    fix: add jest config

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  projects: [
+    './nuclide/atom-test-runners/jest-atom-runner/lib/jest.config.atom.js',
+    './nuclide/atom-test-runners/jest-atom-runner/lib/jest.config.node.js',
+    // './nuclide/atom-test-runners/jest-atom-runner/lib/jest.config.e2e.js', // TODO fb-e2e is missing
+  ],
+};
