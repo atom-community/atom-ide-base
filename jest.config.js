2248fabe16a89311720b7287ae50ae7627c1562b
aminya <aminyahyaabadi74@gmail.com>
2020-10-17 20:49:13 -0500

    fix: move jest.config.js

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  projects: [
-    './nuclide/atom-test-runners/jest-atom-runner/lib/jest.config.atom.js',
-    './nuclide/atom-test-runners/jest-atom-runner/lib/jest.config.node.js',
-    // './nuclide/atom-test-runners/jest-atom-runner/lib/jest.config.e2e.js', // TODO fb-e2e is missing
-  ],
-};
---
+++ nuclide/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  projects: [
+    __dirname + '/atom-test-runners/jest-atom-runner/lib/jest.config.atom.js',
+    __dirname + '/atom-test-runners/jest-atom-runner/lib/jest.config.node.js',
+    // __dirname + '/atom-test-runners/jest-atom-runner/lib/jest.config.e2e.js', // TODO fb-e2e is missing
+  ],
+};
