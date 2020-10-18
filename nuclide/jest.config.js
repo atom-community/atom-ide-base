cf75c0ae2c4aaea56a003b00850d66f2f64fd2bc
aminya <aminyahyaabadi74@gmail.com>
2020-10-17 21:25:58 -0500

    chore: rename lib to configs

--- nuclide/jest.config.js
+++ nuclide/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   projects: [
-    __dirname + '/atom-test-runners/jest-atom-runner/lib/jest.config.atom.js',
-    __dirname + '/atom-test-runners/jest-atom-runner/lib/jest.config.node.js',
-    // __dirname + '/atom-test-runners/jest-atom-runner/lib/jest.config.e2e.js', // TODO fb-e2e is missing
+    __dirname + '/atom-test-runners/jest-atom-runner/configs/jest.config.atom.js',
+    __dirname + '/atom-test-runners/jest-atom-runner/configs/jest.config.node.js',
+    // __dirname + '/atom-test-runners/jest-atom-runner/configs/jest.config.e2e.js', // TODO fb-e2e is missing
   ],
 };
