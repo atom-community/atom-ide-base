4489648fcb29cb990e162229b977e6c6f862f0a2
aminya <aminyahyaabadi74@gmail.com>
2020-10-17 23:37:30 -0500

    fix: seprate jest test scripts and config files for node and atom

--- nuclide/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  projects: [
-    __dirname + '/atom-test-runners/jest-atom-runner/configs/jest.config.atom.js',
-    __dirname + '/atom-test-runners/jest-atom-runner/configs/jest.config.node.js',
-    // __dirname + '/atom-test-runners/jest-atom-runner/configs/jest.config.e2e.js', // TODO fb-e2e is missing
-  ],
-};
