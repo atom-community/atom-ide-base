a78b330252402aed774727485130cfd4efd02ca4
aminya <aminyahyaabadi74@gmail.com>
2020-10-18 17:34:52 -0500

    fix: do not transform symbol-definition-preview-sample.js

--- babel.config.js
+++ babel.config.js
@@ -12,6 +12,10 @@ let plugins = []
 module.exports = {
   presets: presets,
   plugins: plugins,
-  ignore: ["node_modules/**", "nuclide/**/node_modules/**", "dist-nuclide/**/node_modules/**"],
+  ignore: [
+    "node_modules/**",
+    "nuclide/**/node_modules/**", "dist-nuclide/**/node_modules/**", // do not transform node_modules
+    "**/fixtures/symbol-definition-preview-sample.js" // should be used as it is
+  ],
   sourceMap: "inline",
 }
