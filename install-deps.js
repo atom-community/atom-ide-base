8ab43aa3d23b80a0c99bfdc594b1a70deae354aa
Tony Brix <tony@brix.ninja>
2021-02-04 00:21:12 -0600

    inherit stdio

--- install-deps.js
+++ install-deps.js
@@ -3,5 +3,5 @@ const pkg = require("./package.json");
 const deps = Array.from(pkg["package-deps"]).map(d => d.name);
 for (const dep of deps) {
 	console.log(`apm install ${dep}`);
-	spawnSync(`apm install ${dep}`, {shell: true})
+	spawnSync(`apm install ${dep}`, {shell: true, stdio: "inherit"})
 }
