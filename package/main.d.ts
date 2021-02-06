5e5a4cef01478c9d8772b245219c791f4646ff92
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-02-05 21:11:36 -0600

    chore: add built-files

--- package/main.d.ts
+++ package/main.d.ts
@@ -1,2 +1,8 @@
+/**
+ * called by Atom when activating an extension
+ */
 export declare function activate(): void;
+/**
+ * called by Atom when deactivating an extension
+ */
 export declare function deactivate(): void;
