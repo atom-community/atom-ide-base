c6bb4b92bb9f6667f34bbfb931f28a9ddb6e6d41
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-03-18 07:31:48 -0500

    fix: add @types packages to the dependencies

--- types-packages/markdown-service.d.ts
+++ types-packages/markdown-service.d.ts
@@ -1,28 +1,6 @@
-export interface DOMPurifyConfig {
-  ADD_ATTR?: string[]
-  ADD_DATA_URI_TAGS?: string[]
-  ADD_TAGS?: string[]
-  ALLOW_DATA_ATTR?: boolean
-  ALLOWED_ATTR?: string[]
-  ALLOWED_TAGS?: string[]
-  FORBID_ATTR?: string[]
-  FORBID_TAGS?: string[]
-  FORCE_BODY?: boolean
-  KEEP_CONTENT?: boolean
-  /**
-   * This defaults to `true` starting DOMPurify 2.2.0. Note that setting it to `false`
-   * might cause XSS from attacks hidden in closed shadowroots in case the browser
-   * supports Declarative Shadow: DOM https://web.dev/declarative-shadow-dom/
-   */
-  RETURN_DOM_IMPORT?: boolean
-  SANITIZE_DOM?: boolean
-  WHOLE_DOCUMENT?: boolean
-  ALLOWED_URI_REGEXP?: RegExp
-  SAFE_FOR_TEMPLATES?: boolean
-  ALLOW_UNKNOWN_PROTOCOLS?: boolean
-  USE_PROFILES?: false | { mathMl?: boolean; svg?: boolean; svgFilters?: boolean; html?: boolean }
-  IN_PLACE?: boolean
-}
+import { Config } from "dompurify"
+export type DOMPurifyConfig = Omit<Config, "RETURN_DOM" | "RETURN_DOM_FRAGMENT" | "RETURN_TRUSTED_TYPE">
+
 /**
  * the markdown service object
  */
