6804d5885aa0dfe93e70f0ccd1e2fe23b8350414
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-04-03 16:02:58 -0500

    feat: lineLengthIfLongerThan

---
+++ src-commons-atom/editor.ts
@@ -0,0 +1,22 @@
+import { TextEditor } from "atom"
+/**
+ * Find if an editor has a line that is longer than the given threashold
+ * @param editor
+ * @param threashold lineLengthForRow threashold
+ * @param lineCount count up to this line. Default is {editor.getLineCount()}
+ * @returns the first line length that has a length larger than threashold. If no line is found, it returns 0
+ */
+export function lineLengthIfLongerThan(
+  editor: TextEditor,
+  threashold: number,
+  lineCount: number = editor.getLineCount()
+) {
+  const buffer = editor.getBuffer()
+  for (let i = 0, len = lineCount; i < len; i++) {
+    const lineLength = buffer.lineLengthForRow(i)
+    if (lineLength > threashold) {
+      return lineLength
+    }
+  }
+  return 0 // small file
+}
