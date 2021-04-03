796b1a51a0e2d6e9f9af8fb13cbdca12b3f70db5
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-04-03 16:10:41 -0500

    feat: largeness

--- src-commons-atom/editor.ts
+++ src-commons-atom/editor.ts
@@ -1,9 +1,36 @@
 import { TextEditor } from "atom"
 
+// TODO move these to atom-ide-base
+const longLineLengthDefault = atom.config.get("linter-ui-default.longLineLength") || 4000
+const largeLineCountDefault = atom.config.get("linter-ui-default.largeFileLineCount") / 6 || 3000 // minimum number of lines to trigger large file optimizations
+
+/**
+ * Find if an editor's largeness based on the given threashold
+ * @param editor
+ * @param largeLineCount lineCountIfLarge threashold
+ * @param longLineLength lineLengthIfLongerThan threashold
+ * @returns the the largness score if editor is large. Otherwise it returns 0
+ */
+export function largeness(
+  editor: TextEditor,
+  largeLineCount: number = largeLineCountDefault,
+  longLineLength: number = longLineLengthDefault
+) {
+  const lineCount = lineCountIfLarge(editor, largeLineCount)
+  if (lineCount !== 0) {
+    return lineCount
+  }
+  const longLine = lineLengthIfLongerThan(editor, longLineLength)
+  if (longLine !== 0) {
+    return longLine
+  }
+  return 0
+}
+
 /**
  * Find if an editor has a line that is longer than the given threashold
  * @param editor
- * @param threashold largeFileLineCount threashold
+ * @param threashold largeLineCount threashold
  * @returns the line count if it is larger than threashold. Otherwise it returns 0
  */
 export function lineCountIfLarge(editor: TextEditor, threashold: number) {
