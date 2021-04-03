51befedb67b1c7c6fee9b34253fa8697b230ccc6
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-04-03 16:03:35 -0500

    feat: lineCountIfLarge

--- src-commons-atom/editor.ts
+++ src-commons-atom/editor.ts
@@ -1,4 +1,23 @@
 import { TextEditor } from "atom"
+
+/**
+ * Find if an editor has a line that is longer than the given threashold
+ * @param editor
+ * @param threashold largeFileLineCount threashold
+ * @returns the line count if it is larger than threashold. Otherwise it returns 0
+ */
+export function lineCountIfLarge(editor: TextEditor, threashold: number) {
+  // @ts-ignore
+  if (editor.largeFileMode) {
+    return 100000
+  }
+  const lineCount = editor.getLineCount()
+  if (lineCount >= threashold) {
+    return lineCount
+  }
+  return 0 // small file
+}
+
 /**
  * Find if an editor has a line that is longer than the given threashold
  * @param editor
