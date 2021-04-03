9f498cc2a378078dd231ffcfa9d081f4a56620ea
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-04-03 02:43:19 -0500

    fix: try getting the element using getElement

--- src-commons-ui/isItemVisible.ts
+++ src-commons-ui/isItemVisible.ts
@@ -9,11 +9,12 @@ export function isItemVisible(item: Parameters<ViewRegistry["getView"]>[0]) {
     return false
   }
   // check the HTMLElement itself (important for when the dock/container is visible but the tab is not selected)
-  if ("element" in item) {
-    const element = (item as { element: any }).element
-    if (element instanceof HTMLElement && (element.style.display === "none" || element.hidden)) {
-      return false
-    }
+  // try getting the element
+  const element =
+    (item as { getElement: () => HTMLElement })?.getElement() ?? (item as { element: HTMLElement })?.element
+  if (element !== undefined && !isElementVisible(element)) {
+    return false
+    // if it we can't detect the invisiblity using HTML we need to consider Atom's context so we continue
   }
   const paneContainer = atom.workspace.paneContainerForItem(item)
   // if no container it is not visible
@@ -26,4 +27,16 @@ export function isItemVisible(item: Parameters<ViewRegistry["getView"]>[0]) {
     // it is visible (when paneContainer is not a dock like TextEditor)
     return true
   }
+}
+
+/** A function to detect if an HTMLElement is visible. It doesn't consider the Atom context.
+ * To detect if an item is visible in Atom use {isItemVisible} instead
+ * This is useful to skip code excecution or updating when the element is not visible.
+ * @param element
+ */
+export function isElementVisible(element: HTMLElement) {
+  if (element instanceof HTMLElement && (element.style.display === "none" || element.hidden)) {
+    return false
+  }
+  return true
 }
